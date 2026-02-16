f445896e155cc42da1391e885e7d25490147826d
Delvin <dlvn3jai@gmail.com>
2026-02-17 01:37:13 +0530

    flake added

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Django development shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.python313
+          pkgs.python313Packages.django
+        ];
+      };
+    };
+}
+
