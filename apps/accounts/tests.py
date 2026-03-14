44288019c47395585519ef5ccb37c63c5838be3c
Delvin <dlvn3jai@gmail.com>
2026-03-14 18:20:47 +0530

    feat:CI Implemented

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+class UserTests(TestCase):
+    def test_create_user(self):
+        user = User.objects.create_user(
+            email="test@example.com",
+            name="Test User",
+            password="password123"
+        )
+        self.assertEqual(user.email, "test@example.com")
+        self.assertEqual(user.role, "faculty")
+        self.assertTrue(user.is_active)
+
+    def test_create_superuser(self):
+        admin = User.objects.create_superuser(
+            email="admin@example.com",
+            name="Admin User",
+            password="password123"
+        )
+        self.assertEqual(admin.role, "admin")
+        self.assertTrue(admin.is_superuser)
+        self.assertTrue(admin.is_staff)
